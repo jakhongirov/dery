fd9e366fbdcbb56ff3a2f1cfb7aecd88f9c9660e
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-06-06 12:07:56 +0500

    add

---
+++ src/modules/orders/model.js
@@ -0,0 +1,78 @@
+const { fetch, fetchALL } = require('../../lib/postgres')
+
+const ordersList = (limit, page) => {
+   const QUERY = `
+      SELECT
+         order_product_id,
+         a.user_id,
+         user_name,
+         user_phone,
+         order_total_price,
+         order_started,
+         order_finished,
+         order_create_at
+      FROM
+         orders a
+      INNER JOIN
+         users b
+      ON
+         a.user_id = b.user_id
+      ORDER BY
+         order_id DESC
+      LIMIT ${Number(limit)}
+      OFFSET ${Number((page - 1) * limit)};
+   `;
+
+   return fetchALL(QUERY)
+}
+const foundOrder = (id) => {
+   const QUERY = `
+      SELECT
+         *
+      FROM
+         orders a
+      INNER JOIN
+         users b
+      ON
+         a.user_id = b.user_id   
+      WHERE
+         order_id = $1;
+   `;
+
+   return fetch(QUERY, id)
+}
+const foundProducts = (products_id) => {
+   const productsId = products_id?.map(e => `${e}`).join(', ');
+   const QUERY = `
+      SELECT
+         *
+      FROM
+         products a
+      INNER JOIN 
+         categories b
+      ON
+         a.category_id = b.category_id
+      WHERE
+         ARRAY[product_id::int] && ARRAY[${productsId}];
+   `;
+
+   return fetchALL(QUERY)
+}
+const deleteOrder = (id) => {
+   const QUERY = `
+      DELETE FROM
+         orders
+      WHERE
+         order_id = $1
+      RETURNING *;
+   `
+
+   return fetch(QUERY, id)
+}
+
+module.exports = {
+   ordersList,
+   foundOrder,
+   foundProducts,
+   deleteOrder
+}
---
+++ src/modules/orders/orders.js
@@ -0,0 +1,123 @@
+const model = require('./model')
+
+module.exports = {
+   GET: async (req, res) => {
+      try {
+         const { limit, page } = req.query
+
+         if (limit && page) {
+            const ordersList = await model.ordersList(limit, page)
+
+            if (ordersList?.lenght > 0) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: ordersList
+               })
+            } else {
+               return res.status(404).json({
+                  status: 404,
+                  message: "Not found"
+               })
+            }
+
+         } else {
+            return res.status(400).json({
+               status: 400,
+               message: "Must write limit and page"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   GET_ID: async (req, res) => {
+      try {
+         const { id } = req.params
+
+         if (id) {
+            const foundOrder = await model.foundOrder(id)
+            const foundProducts = await model.foundProducts(foundOrder?.order_product_id)
+            foundOrder['products'] = foundProducts
+            
+            if (foundOrder) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: foundOrder
+               })
+            } else {
+               return res.status(404).json({
+                  status: 404,
+                  message: "Not found"
+               })
+            }
+
+         } else {
+            return res.status(400).json({
+               status: 400,
+               message: "Bad request"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   DELETE: async (req, res) => {
+      try {
+         const { id } = req.body
+
+         if (id) {
+            const foundOrder = await model.foundOrder(id)
+
+            if (foundOrder) {
+               const deleteOrder = await model.deleteOrder(id)
+
+               if (deleteOrder) {
+                  return res.status(200).json({
+                     status: 200,
+                     message: "Success",
+                     data: deleteOrder
+                  })
+               } else {
+                  return res.status(400).json({
+                     status: 400,
+                     message: "Bad request"
+                  })
+               }
+
+            } else {
+               return res.status(404).json({
+                  status: 404,
+                  message: "Not found"
+               })
+            }
+
+         } else {
+            return res.status(400).json({
+               status: 400,
+               message: "Bad request"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   }
+}
